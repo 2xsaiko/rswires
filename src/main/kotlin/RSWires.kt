04d48d2494351cbfc168b271d25dc17041edc051
2xsaiko <git@dblsaiko.net>
2020-09-02 19:07:05 +0200

    Use end world tick event instead of deprecated world tick callback

--- src/main/kotlin/RSWires.kt
+++ src/main/kotlin/RSWires.kt
@@ -5,8 +5,7 @@ import net.dblsaiko.rswires.common.init.BlockEntityTypes
 import net.dblsaiko.rswires.common.init.Blocks
 import net.dblsaiko.rswires.common.init.Items
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.event.world.WorldTickCallback
-import net.minecraft.server.world.ServerWorld
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 
 const val MOD_ID = "rswires"
 
@@ -18,10 +17,8 @@ object RSWires : ModInitializer {
     Blocks.register()
     Items.register()
 
-    WorldTickCallback.EVENT.register(WorldTickCallback {
-      if (it is ServerWorld) {
-        RedstoneWireUtils.flushUpdates(it)
-      }
+    ServerTickEvents.END_WORLD_TICK.register(ServerTickEvents.EndWorldTick {
+      RedstoneWireUtils.flushUpdates(it)
     })
   }
 }
